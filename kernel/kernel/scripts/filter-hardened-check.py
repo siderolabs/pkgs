674881902e2bde5c4b44999b8a8099659df43b50
Andrey Smirnov <smirnov.andrey@gmail.com>
2021-02-18 15:55:57 +0300

    feat: update Linux to 5.10.17, disable init_on_free=1 by default
    KSPP doesn't strictly require init_on_free=1:
    
    ```
    The init_on_free is only needed if there is concern about minimizing stale data lifetime.
    ```
    
    It has significant performance impact:
    
    ```
    Slowdown for the new features compared to init_on_free=0,
    init_on_alloc=0:
    
    hackbench, init_on_free=1:  +7.62% sys time (st.err 0.74%)
    hackbench, init_on_alloc=1: +7.75% sys time (st.err 2.14%)
    
    Linux build with -j12, init_on_free=1:  +8.38% wall time (st.err 0.39%)
    Linux build with -j12, init_on_free=1:  +24.42% sys time (st.err 0.52%)
    Linux build with -j12, init_on_alloc=1: -0.13% wall time (st.err 0.42%)
    Linux build with -j12, init_on_alloc=1: +0.57% sys time (st.err 0.40%)
    ```
    
    And it causes all the RAM to be allocated (touched) on boot which causes
    problems with virtualized environments in memory over-commit scenarios.
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- kernel/kernel/scripts/filter-hardened-check.py
+++ kernel/kernel/scripts/filter-hardened-check.py
@@ -17,6 +17,7 @@ IGNORE_VIOLATIONS = {
     'CONFIG_MODULES', # enabled for backwards compat, modules require signing key which is thrown away
     'CONFIG_IA32_EMULATION', # see https://github.com/talos-systems/pkgs/pull/125
     'CONFIG_HARDEN_BRANCH_PREDICTOR', # looks like a bug in kconfig-hardened-check, default in 5.9, but not enabled in 5.10
+    'CONFIG_INIT_ON_FREE_DEFAULT_ON', # disabled init_on_free=1 due to performance
 }
 
 def main():
