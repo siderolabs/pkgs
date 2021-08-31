982bc18a4ecf5d4e15a73c350abb97f02adb8871
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2021-08-31 21:47:39 +0300

    chore: update tools
    This updates tools to reproducible build enabled ones, bumps toolchain
    indirectly.
    
    Build fixes related to changes in tools/toolchain.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- kernel/kernel/scripts/filter-hardened-check.py
+++ kernel/kernel/scripts/filter-hardened-check.py
@@ -18,6 +18,7 @@ IGNORE_VIOLATIONS = {
     'CONFIG_IA32_EMULATION', # see https://github.com/talos-systems/pkgs/pull/125
     'CONFIG_HARDEN_BRANCH_PREDICTOR', # looks like a bug in kconfig-hardened-check, default in 5.9, but not enabled in 5.10
     'CONFIG_INIT_ON_FREE_DEFAULT_ON', # disabled init_on_free=1 due to performance
+    'CONFIG_ARM64_EPAN', # not available in 5.10, first introduced in 5.13
 }
 
 def main():
