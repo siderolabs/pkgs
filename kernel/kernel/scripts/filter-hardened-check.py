f307e64e6b2e8a29cff5508ae2da4ae92286771e
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2021-11-10 15:11:22 +0300

    feat: update Linux to 5.15.1
    5.15 is the new LTS: https://www.kernel.org/category/releases.html
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- kernel/kernel/scripts/filter-hardened-check.py
+++ kernel/kernel/scripts/filter-hardened-check.py
@@ -18,8 +18,6 @@ IGNORE_VIOLATIONS = {
     'CONFIG_IA32_EMULATION', # see https://github.com/talos-systems/pkgs/pull/125
     'CONFIG_HARDEN_BRANCH_PREDICTOR', # looks like a bug in kconfig-hardened-check, default in 5.9, but not enabled in 5.10
     'CONFIG_INIT_ON_FREE_DEFAULT_ON', # disabled init_on_free=1 due to performance
-    'CONFIG_ARM64_EPAN', # not available in 5.10, first introduced in 5.13
-    'CONFIG_RANDOMIZE_KSTACK_OFFSET_DEFAULT', # not available in 5.10, first introduced in 5.13
 }
 
 def main():
