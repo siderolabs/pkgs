f4335dc45bc08910fc2ccdd429deb96acd602c48
Noel Georgi <git@frezbo.dev>
2024-03-18 17:45:20 +0530

    fix: kernel hardening check script
    Fix the script to handle new upstream format.
    Also handle KSPP failures and optionally enable some features and add
    others to whitelist.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- kernel/build/scripts/filter-hardened-check.py
+++ kernel/build/scripts/filter-hardened-check.py
@@ -16,21 +16,32 @@ Names of violations we ignore for a good reason.
 IGNORE_VIOLATIONS = {
     'CONFIG_MODULES', # enabled for backwards compat, modules require signing key which is thrown away
     'CONFIG_IA32_EMULATION', # see https://github.com/siderolabs/pkgs/pull/125
-    'CONFIG_HARDEN_BRANCH_PREDICTOR', # looks like a bug in kconfig-hardened-check, default in 5.9, but not enabled in 5.10
+    'CONFIG_COMPAT', # enabled when CONFIG_IA32_EMULATION is enabled
     'CONFIG_INIT_ON_FREE_DEFAULT_ON', # disabled init_on_free=1 due to performance
+    'CONFIG_BINFMT_MISC', # build as module, can only be loaded explicitly
+    'CONFIG_WERROR', # breaks downstream kernel modules build such as drbd
+    'CONFIG_DEBUG_VIRTUAL', # disabled due to performance reasons
+    'CONFIG_STATIC_USERMODEHELPER', # disabled until further research is done, see https://github.com/siderolabs/pkgs/issues/918
+    'CONFIG_LOCK_DOWN_KERNEL_FORCE_CONFIDENTIALITY', # disabled until further research is done, see https://github.com/siderolabs/pkgs/issues/918
+    'CONFIG_RANDSTRUCT_FULL', # disabled due to performance reasons
+    'CONFIG_RANDSTRUCT_PERFORMANCE', # disabled due to performance reasons
+    'CONFIG_UBSAN_TRAP', # disabled due to performance reasons
+    'CONFIG_CFI_CLANG', # SideroLabs toolchain uses gcc, investigae more, see https://github.com/siderolabs/pkgs/issues/918
+    'CONFIG_CFI_PERMISSIVE', # SideroLabs toolchain uses gcc, investigae more, see https://github.com/siderolabs/pkgs/issues/918
+    'CONFIG_ARM64_BTI_KERNEL', # can't seem to enable this, probably because we're using gcc, see https://github.com/siderolabs/pkgs/issues/918
 }
 
 def main():
     violations = json.load(sys.stdin)
 
     # filter out non-failures
-    violations = [item for item in violations if item[4].startswith("FAIL")]
+    violations = [item for item in violations if item["check_result"].startswith("FAIL")]
 
     # filter only failures in the groups we're interested in
-    violations = [item for item in violations if item[2] in GROUPS]
+    violations = [item for item in violations if item["decision"] in GROUPS]
 
     # filter out violations we ignore
-    violations = [item for item in violations if item[0] not in IGNORE_VIOLATIONS]
+    violations = [item for item in violations if item["option_name"] not in IGNORE_VIOLATIONS]
 
     if not violations:
         sys.exit(0)
@@ -38,8 +49,8 @@ def main():
     print('{:^45}|{:^13}|{:^10}|{:^20}'.format('option name', 'desired val', 'decision', 'reason'))
     print('=' * 91)
 
-    for violation in violations:
-        print('{:<45}|{:^13}|{:^10}|{:^20}'.format(*violation))
+    for item in violations:
+        print('{:<45}|{:^13}|{:^10}|{:^20}'.format(item["option_name"], item["desired_val"], item["decision"],item["reason"]))
 
     sys.exit(1)
 
