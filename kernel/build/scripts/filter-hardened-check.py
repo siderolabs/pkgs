0f4cbbcf892f88b988580fb16a539ddde2a1afbd
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2025-07-08 14:47:36 +0400

    feat: update dependencies
    ```
    | Package | Update | Change |
    |---|---|---|
    | [amzn/amzn-drivers](https://redirect.github.com/amzn/amzn-drivers) | minor | `2.14.1` -> `2.15.0` |
    | git://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git | minor | `6.14.0` -> `6.15.0` |
    | git://git.kernel.org/pub/scm/utils/cryptsetup/cryptsetup.git | minor | `2.7.5` -> `2.8.0` |
    | git://sourceware.org/git/lvm2.git | patch | `2_03_32` -> `2_03_33` |
    | https://github.com/a13xp0p0v/kernel-hardening-checker.git | digest | `0c65440` -> `0f6c72e` |
    | https://github.com/ipxe/ipxe.git | digest | `5b3ebf8` -> `c21443f` |
    | [https://gitlab.gnome.org/GNOME/glib.git](https://gitlab.gnome.org/GNOME/glib) | patch | `2.85.0` -> `2.85.1` |
    | [systemd/systemd](https://redirect.github.com/systemd/systemd) | minor | `257.6` -> `257.7` |
    ```
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- kernel/build/scripts/filter-hardened-check.py
+++ kernel/build/scripts/filter-hardened-check.py
@@ -42,6 +42,7 @@ IGNORE_VIOLATIONS_BY_ARCH = {
     'arm64': {
         'CONFIG_ARM64_BTI_KERNEL', # can't seem to enable this, probably because we're using gcc, see https://github.com/siderolabs/pkgs/issues/918
         'CONFIG_UNWIND_PATCH_PAC_INTO_SCS', # this is a Clang feature, we use gcc
+        'CONFIG_DEFAULT_MMAP_MIN_ADDR', # looks to be a bug in the kernel-hardening-checker, the config is set in kernel config
     },
     'amd64': {},
 }
