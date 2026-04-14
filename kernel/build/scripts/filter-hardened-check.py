34de6dbcd3eba54a10f7d28670b598069b9b9bcd
Noel Georgi <git@frezbo.dev>
2026-04-15 00:39:54 +0530

    fix: support disabling module signature verification
    Even though https://github.com/siderolabs/talos/issues/11989 got completed, it missed
    a critical step to disable `CONFIG_MODULE_SIG_FORCE` as the issues states.
    This got missed and released, fixing the errata.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>
    (cherry picked from commit 6ea49c7264baf6948e8b793f0b8c1306f71efe5a)

--- kernel/build/scripts/filter-hardened-check.py
+++ kernel/build/scripts/filter-hardened-check.py
@@ -34,6 +34,7 @@ IGNORE_VIOLATIONS = {
     'CONFIG_INET_DIAG', # last vulnerability prior to v4.1. Required for CNIs such as Cilium to terminate sockets. (https://github.com/siderolabs/pkgs/issues/1028)
     'CONFIG_IOMMU_DEFAULT_DMA_STRICT', # performance impact https://github.com/siderolabs/talos/issues/9531
     'CONFIG_PROC_MEM_NO_FORCE', # might break some applications, so instead we will enforce in the kernel arg 'proc_mem.force_override=never' (https://github.com/a13xp0p0v/kernel-hardening-checker/pull/201)
+    'CONFIG_MODULE_SIG_FORCE', # see https://github.com/siderolabs/talos/issues/11989
 }
 
 """
